75c7ec599313e335c1be49b050ae9c5ced4acb9b
이준호 <115637281+codingMachineJunior@users.noreply.github.com>
2023-08-30 22:45:58 +0900

    Update 폰켓몬.java

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -6,19 +6,10 @@ class Solution {
         
         Map <Integer,Integer> map = new HashMap<>();
         
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++)
             map.put(nums[i],1);
-        }
         
-        map.forEach((key,value)->{
-           System.out.println(key+" "+value); 
-        });
-        
-        if(map.size()>=nums.length/2)
-            answer = nums.length/2;
-        else
-            answer = map.size();
-        
+        answer = map.size()>=nums.length/2 ? nums.length/2 : map.size();     
         return answer;
     }
 }
