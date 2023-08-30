cb7ee74bb1a111028914cd83fac5352bfff5d7f4
이준호 <115637281+codingMachineJunior@users.noreply.github.com>
2023-08-30 21:55:37 +0900

    [level 1] Title: 폰켓몬, Time: 16.55 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Map <Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i],1);
+        }
+        
+        map.forEach((key,value)->{
+           System.out.println(key+" "+value); 
+        });
+        
+        if(map.size()>=nums.length/2)
+            answer = nums.length/2;
+        else
+            answer = map.size();
+        
+        return answer;
+    }
+}
